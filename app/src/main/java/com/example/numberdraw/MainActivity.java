aff248ca20d3a14acdaf9730ee5a8717f6398394
Robson Marinho <robson_luz15@hotmail.com>
2019-01-24 23:18:03 -0300

    Adicionando evento de clique no botão(ClickHere)

--- app/src/main/java/com/example/numberdraw/MainActivity.java
+++ app/src/main/java/com/example/numberdraw/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.numberdraw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void changeText(View view){
+
+        TextView text = (TextView) findViewById(R.id.textShow);
+        text.setText("Text changed");
+
+    }
+
+
 }
